faec15f7d74fd2bb00ffa9b7c723683200808271
Samuel Morales Jr <smoralesjr@gmail.com>
2018-09-28 20:07:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const updateObjectWithKeyAndValue = function (object, key, value){
 const destructivelyUpdateObjectWithKeyAndValue = function (object, key, value){
   
 };
+
+
+const deleteFromObjectByKey = function 
