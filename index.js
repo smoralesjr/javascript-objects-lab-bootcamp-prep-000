4712cb2b7000ac254bee6d1a8d34aa4b58a7af98
Samuel Morales Jr <smoralesjr@gmail.com>
2018-09-28 20:08:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const updateObjectWithKeyAndValue = function (object, key, value){
+const updateObjectWithKeyAndValue = function(object, key, value){
   
 };
 
 
 
-const destructivelyUpdateObjectWithKeyAndValue = function (object, key, value){
+const destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
   
 };
 
 
-const deleteFromObjectByKey = function 
+const deleteFromObjectByKey = function(object, key){
+  
+};
